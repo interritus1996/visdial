b4460d7a7df18362cc946d4a76af0398395ec690
interritus1996 <shrivastava.ayush1996@gmail.com>
2017-09-23 14:51:07 +0530

    New Model

---
+++ encoders/newModel.lua
@@ -0,0 +1,67 @@
+local encoderNet = {}
+
+function encoderNet.model(params)
+
+    local dropout = 0.5
+
+    local inputs = {}
+    local outputs = {}
+
+    table.insert(inputs, nn.Identity()()) -- question
+    table.insert(inputs, nn.Identity()()) -- img feats
+    table.insert(inputs, nn.Identity()()) -- history
+    -- table.insert(inputs, nn.Identity()()) -- 10x10 mask
+
+    local ques = inputs[1]
+    local img_feats = inputs[2]
+    local hist = inputs[3]
+    -- local mask = inputs[4]
+
+    -- word embed layer
+    wordEmbed = nn.LookupTableMaskZero(params.vocabSize, params.embedSize);
+
+    -- make clones for embed layer
+    local qEmbed = nn.Dropout(dropout)(wordEmbed:clone('weight', 'bias', 'gradWeight', 'gradBias')(ques));
+    local hEmbed = nn.Dropout(dropout)(wordEmbed:clone('weight', 'bias', 'gradWeight', 'gradBias')(hist));
+
+    local lst1 = nn.SeqLSTM(params.embedSize, params.rnnHiddenSize)
+    lst1:maskZero()
+
+    local lst2 = nn.SeqLSTM(params.rnnHiddenSize, params.rnnHiddenSize)
+    lst2:maskZero()
+
+    local h1 = lst1(hEmbed)
+    local h2 = lst2(h1)
+    local h3 = nn.Select(1, -1)(h2)
+
+    local lst3 = nn.SeqLSTM(params.embedSize, params.rnnHiddenSize)
+    lst3:maskZero()
+
+    local lst4 = nn.SeqLSTM(params.rnnHiddenSize, params.rnnHiddenSize)
+    lst4:maskZero()
+
+    local q1 = lst3(qEmbed)
+    local q2 = lst4(q1)
+    local q3 = nn.Select(1, -1)(q2)
+    local q3_view = nn.View(-1,512,1):setNumInputDims(2)(q3)
+
+    local img_feats_view = nn.Transpose({1,2})(nn.View(-1,params.batchSize*10,512)(nn.Transpose({1,2},{2,3})(img_feats)))
+    local mm = nn.MM(){img_feats_view,q3_view}
+    local prob = nn.SoftMax()(nn.View(-1,196)(mm))
+    local prob_view = nn.View(-1,196,1)(prob)
+
+    local mm1 = nn.MM(true,false){prob_view,img_feats_view}
+    local mm1_view = nn.View(-1,512)(mm1)
+
+    local q_join = nn.JoinTable(1,1)({mm1_view,q3,h3})
+    local final_layer = nn.Tanh()(nn.Linear(1536,512)(nn.Dropout(dropout)(q_join)))
+
+    table.insert(outputs, final_layer)
+
+    local enc = nn.gModule(inputs, outputs)
+    enc.wordEmbed = wordEmbed
+
+    return enc;
+end
+
+return encoderNet
